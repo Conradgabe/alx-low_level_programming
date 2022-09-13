83ef81dcd7c68ea12dc3dcf3d196c2134a8219ab
Conradgabe <gisuekebho5880@gmail.com>
2022-09-13 02:29:13 -0700

    shell scripting:print the alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** 
+ * print_alphabet - the entry point
+ *
+ * Description: prints the alphabet in lowercase
+ * Return: int
+ */
+void print_alphabet(void)
+{
+	int alphabet = 'a';
+
+	while (alphabet < 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar("\n");
+}
