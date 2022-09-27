0b8c0a83bc4b7ac9ed8e29c524a97a1083a53ae4
Conradgabe <gisuekebho5880@gmail.com>
2022-09-27 12:46:34 -0700

    shell scripting in c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 /**
  * File: main.h
- * Auth: Firdaus
+ * Auth: Gabriel
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-pointers_arrays_strings directory.
  */
-
-#include <stddef.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -21,4 +17,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
